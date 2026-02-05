34a7ead997a9ec6b8cf4a22feecf4220eb9262d3
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-02-05 15:52:35 -0300

    Base de datos instanciada en clase principal

--- develop/app/src/main/java/com/devs/tutorsapp/TeachMeApplication.java
+++ develop/app/src/main/java/com/devs/tutorsapp/TeachMeApplication.java
@@ -1,21 +1,28 @@
 package com.devs.tutorsapp;
 
 import android.app.Application;
+import com.devs.tutorsapp.data.local.database.AppDatabase;
 
 public class TeachMeApplication extends Application {
 
     private static TeachMeApplication instance;
+    private AppDatabase database;
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
-
         android.util.Log.d("devtest", "Application iniciada");
+
+        database = AppDatabase.getInstance(this);
+        android.util.Log.d("devtest", "Base de datos inicializada");
     }
 
     public static TeachMeApplication getInstance() {
         return instance;
     }
 
+    public AppDatabase getDatabase() {
+        return database;
+    }
 }
