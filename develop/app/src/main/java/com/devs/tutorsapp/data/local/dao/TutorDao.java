7969069b059b37ad065b27a099c100dca7f60b14
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-02-11 16:33:23 -0300

    Dao agregado para consultas de Tutores

---
+++ develop/app/src/main/java/com/devs/tutorsapp/data/local/dao/TutorDao.java
@@ -0,0 +1,31 @@
+package com.devs.tutorsapp.data.local.dao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import androidx.room.Delete;
+import com.devs.tutorsapp.data.local.entity.TutorEntity;
+import java.util.List;
+import androidx.lifecycle.LiveData;
+
+
+@Dao
+public interface TutorDao {
+
+    @Insert
+    long insertTutor(TutorEntity tutor);
+
+    @Update
+    void updateTutor(TutorEntity tutor);
+
+    @Delete
+    void deleteTutor(TutorEntity tutor);
+
+    @Query("SELECT * FROM tutores")
+    LiveData<List<TutorEntity>> getAllTutores();
+
+    @Query("SELECT * FROM tutores WHERE tutor_id = :tutor_id")
+    TutorEntity getById(int tutor_id);
+
+}
