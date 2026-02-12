c0e46a116ea0841b6ffe82030eb92ad4a391c6fb
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-02-12 16:55:30 -0300

    ViewModel agregado para clases

---
+++ develop/app/src/main/java/com/devs/tutorsapp/ui/viewmodel/ClaseViewModel.java
@@ -0,0 +1,49 @@
+package com.devs.tutorsapp.ui.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.annotation.NonNull;
+import android.app.Application;
+import android.util.Log;
+
+import com.devs.tutorsapp.data.local.entity.ClaseEntity;
+import com.devs.tutorsapp.data.repository.ClaseRepository;
+import com.devs.tutorsapp.data.repository.RepositoryCallback;
+
+import java.util.List;
+
+
+public class ClaseViewModel extends AndroidViewModel {
+
+    private final ClaseRepository repository;
+    private final MutableLiveData<List<ClaseEntity>> listaClases = new MutableLiveData<>();
+
+    public ClaseViewModel(@NonNull Application application) {
+        super(application);
+        repository = new ClaseRepository(application);
+    }
+
+    public LiveData<List<ClaseEntity>> getListaClases() {
+        return listaClases;
+    }
+
+    public void cargarClasesPorAlumno(int alumnoId) {
+        repository.obtenerClasesPorAlumno(alumnoId, new RepositoryCallback<List<ClaseEntity>>() {
+            @Override
+            public void onSuccess(List<ClaseEntity> result) {
+                listaClases.postValue(result);
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.d("devtest", "Error al cargar las clases");
+            }
+        });
+    }
+
+    public void crearClase(ClaseEntity clase) {
+        repository.crearClase(clase);
+    }
+
+}
