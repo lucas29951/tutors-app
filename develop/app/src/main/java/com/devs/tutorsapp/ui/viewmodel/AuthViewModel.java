caa3de78a9a02b5fa54b5ef216f7893651eaf83d
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-02-12 16:29:08 -0300

    Paquete UI y ViewModels agregados

---
+++ develop/app/src/main/java/com/devs/tutorsapp/ui/viewmodel/AuthViewModel.java
@@ -0,0 +1,60 @@
+package com.devs.tutorsapp.ui.viewmodel;
+
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.devs.tutorsapp.data.repository.AuthCallback;
+import com.devs.tutorsapp.data.repository.AuthRepository;
+
+
+public class AuthViewModel extends AndroidViewModel {
+
+    private final AuthRepository repository;
+    private final MutableLiveData<String> successMessage = new MutableLiveData<>();
+    private final MutableLiveData<String> errorMessage = new MutableLiveData<>();
+
+    public AuthViewModel(@NonNull Application application) {
+        super(application);
+        repository = new AuthRepository(application);
+    }
+
+    public LiveData<String> getSuccessMessage() {
+        return successMessage;
+    }
+
+    public LiveData<String> getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void registrarAlumno(String nombre, String apellido, String email, String password, String telefono, String direccion, String foto) {
+        repository.registrarAlumno(nombre, apellido, email, password, telefono, direccion, foto, new AuthCallback() {
+            @Override
+            public void onSuccess(String message) {
+                successMessage.postValue(message);
+            }
+
+            @Override
+            public void onError(String error) {
+                errorMessage.postValue(error);
+            }
+        });
+    }
+
+    public void login(String email, String password) {
+        repository.login(email, password, new AuthCallback() {
+            @Override
+            public void onSuccess(String message) {
+                successMessage.postValue(message);
+            }
+
+            @Override
+            public void onError(String error) {
+                errorMessage.postValue(error);
+            }
+        });
+    }
+
+}
