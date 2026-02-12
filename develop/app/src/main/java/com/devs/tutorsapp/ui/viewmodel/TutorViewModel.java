0a111d02c64aa8d60b0496cdfec76ea3eed8dc10
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-02-12 16:55:05 -0300

    ViewModel agregado para Tutores

---
+++ develop/app/src/main/java/com/devs/tutorsapp/ui/viewmodel/TutorViewModel.java
@@ -0,0 +1,45 @@
+package com.devs.tutorsapp.ui.viewmodel;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.devs.tutorsapp.data.local.entity.TutorEntity;
+import com.devs.tutorsapp.data.repository.RepositoryCallback;
+import com.devs.tutorsapp.data.repository.TutorRepository;
+
+import java.util.List;
+
+public class TutorViewModel extends AndroidViewModel {
+
+    private final TutorRepository repository;
+    private final MutableLiveData<List<TutorEntity>> listaTutores = new MutableLiveData<>();
+
+    public TutorViewModel(@NonNull Application application) {
+        super(application);
+        repository = new TutorRepository(application);
+    }
+
+    public LiveData<List<TutorEntity>> getListaTutores() {
+        return listaTutores;
+    }
+
+    public void cargarTutores() {
+        repository.obtenerTutores(new RepositoryCallback<List<TutorEntity>>() {
+            @Override
+            public void onSuccess(List<TutorEntity> result) {
+                listaTutores.postValue(result);
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.d("devtest", "Error al cargar los tutores");
+            }
+        });
+    }
+
+}
