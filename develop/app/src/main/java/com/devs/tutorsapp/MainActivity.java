bbe513675680c9233f1c199090a569a64887044d
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-02-13 16:34:03 -0300

    Login de la vista principal añadida

--- develop/app/src/main/java/com/devs/tutorsapp/MainActivity.java
+++ develop/app/src/main/java/com/devs/tutorsapp/MainActivity.java
@@ -1,24 +1,35 @@
 package com.devs.tutorsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.devs.tutorsapp.ui.auth.LoginActivity;
+import com.devs.tutorsapp.ui.auth.RegistroActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignIn, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnSignIn.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        btnRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
+            startActivity(intent);
         });
     }
 }
