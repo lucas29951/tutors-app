bacbfb4e925e72389aa3f7e0494a1a0b766d8542
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-02-13 16:35:17 -0300

    Logica añadida para la vista de Login

--- develop/app/src/main/java/com/devs/tutorsapp/ui/auth/LoginActivity.java
+++ develop/app/src/main/java/com/devs/tutorsapp/ui/auth/LoginActivity.java
@@ -1,26 +1,69 @@
 package com.devs.tutorsapp.ui.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import androidx.lifecycle.ViewModelProvider;
+
+import com.devs.tutorsapp.MainActivity;
 import com.devs.tutorsapp.R;
+import com.devs.tutorsapp.ui.viewmodel.AuthViewModel;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private AuthViewModel authViewModel;
+    private EditText etEmail, etPassword;
+    private Button btnLogin;
+    private TextView tvRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvRegistro = findViewById(R.id.tvSignUp);
+
+        btnLogin.setOnClickListener(v -> login());
+
+        tvRegistro.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
+            startActivity(intent);
         });
+
+        authViewModel.getSuccessMessage().observe(this, message -> {
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        authViewModel.getErrorMessage().observe(this, error -> {
+            Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void login() {
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Completa los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        authViewModel.login(email, password);
     }
 }
