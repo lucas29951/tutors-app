13b591def37b32b0f79b041a6b76deadba94c5c4
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-02-13 17:41:30 -0300

    Metodo de precarga de la BD agregado

--- develop/app/src/main/java/com/devs/tutorsapp/data/local/database/AppDatabase.java
+++ develop/app/src/main/java/com/devs/tutorsapp/data/local/database/AppDatabase.java
@@ -2,9 +2,11 @@ package com.devs.tutorsapp.data.local.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.devs.tutorsapp.data.local.dao.AlumnoDao;
 import com.devs.tutorsapp.data.local.dao.ClaseDao;
@@ -21,6 +23,8 @@ import com.devs.tutorsapp.data.local.entity.ResenaEntity;
 import com.devs.tutorsapp.data.local.entity.TutorEntity;
 import com.devs.tutorsapp.data.local.entity.TutorMateriaEntity;
 
+import java.util.concurrent.Executors;
+
 @Database(
         entities = {
                 AlumnoEntity.class,
@@ -36,7 +40,7 @@ import com.devs.tutorsapp.data.local.entity.TutorMateriaEntity;
 )
 public abstract class AppDatabase extends RoomDatabase {
 
-    private static AppDatabase INSTANCE;
+    private static volatile AppDatabase INSTANCE;
 
     public abstract AlumnoDao alumnoDao();
     public abstract TutorDao tutorDao();
@@ -52,10 +56,45 @@ public abstract class AppDatabase extends RoomDatabase {
                     context.getApplicationContext(),
                     AppDatabase.class,
                     "teachme_database"
-            ).fallbackToDestructiveMigration().build();
+            )
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build();
             android.util.Log.d("devtest", "Base de datos creada");
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            Executors.newSingleThreadExecutor().execute(() -> {
+                AppDatabase database = INSTANCE;
+
+                prepopulate(database);
+            });
+        }
+    };
+
+    private static void prepopulate(AppDatabase database) {
+
+        MateriaEntity matematica = new MateriaEntity("Matematica", "Clases de Algebra y Analisis Matematico");
+        MateriaEntity programacion = new MateriaEntity("Programacion", "Java y Desarrollo Android");
+
+        database.materiaDao().insertMateria(matematica);
+        database.materiaDao().insertMateria(programacion);
+
+        TutorEntity tutor1 = new TutorEntity("Carlos", "Gomez", "carlos@gmail.com", "123456789", "Profesor de matematica con 10 años de experiencia.", "", "Buenos Aires 123");
+        TutorEntity tutor2 = new TutorEntity("Laura", "Martinez", "laura@gmail.com", "987654321", "Desarrolladora Android y profesora universitaria.", "", "Cordoba 456");
+
+        long tutor1Id = database.tutorDao().insertTutor(tutor1);
+        long tutor2Id = database.tutorDao().insertTutor(tutor2);
+
+        database.tutorMateriaDao().insertTutorMateria(new TutorMateriaEntity((int) tutor1Id, 1));
+        database.tutorMateriaDao().insertTutorMateria(new TutorMateriaEntity((int) tutor2Id, 2));
+
     }
 
 }
