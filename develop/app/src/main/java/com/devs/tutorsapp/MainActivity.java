8823cd12e622fa3c92dca75c793a7b490e4988a3
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-02-17 20:16:22 -0300

    Modificacion: chequeo de estados de primera vez y sesion activa

--- develop/app/src/main/java/com/devs/tutorsapp/MainActivity.java
+++ develop/app/src/main/java/com/devs/tutorsapp/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.devs.tutorsapp.ui.auth.LoginActivity;
 import com.devs.tutorsapp.ui.auth.RegistroActivity;
+import com.devs.tutorsapp.ui.home.HomeActivity;
+import com.devs.tutorsapp.utils.SharedPrefManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,19 +19,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        SharedPrefManager prefs = SharedPrefManager.getInstance(this);
 
+        if (prefs.isFirstTime()) {
+            prefs.setFirstTimeFalse();
+            setContentView(R.layout.activity_main);
+            initViews();
+            return;
+        }
+
+        if (prefs.isLoggedIn()) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            intent.putExtra("idAlumno", prefs.getAlumnoId());
+            intent.putExtra("nombreAlumno", prefs.getAlumnoNombre());
+            intent.putExtra("emailAlumno", prefs.getAlumnoEmail());
+            startActivity(intent);
+            finish();
+            return;
+        }
+
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+        return;
+    }
+
+    private void initViews() {
         btnSignIn = findViewById(R.id.btnSignIn);
         btnRegister = findViewById(R.id.btnRegister);
 
         btnSignIn.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
+            finish();
+            return;
         });
 
         btnRegister.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
             startActivity(intent);
+            finish();
+            return;
         });
     }
 }
