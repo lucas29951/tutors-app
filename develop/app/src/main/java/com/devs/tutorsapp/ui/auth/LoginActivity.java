88da423dbd9fbf6e0b09953259b2f4d5097277a8
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-02-17 20:17:52 -0300

    LoginActivity modificado: manejo de guardado y chequeo de sesion activa

--- develop/app/src/main/java/com/devs/tutorsapp/ui/auth/LoginActivity.java
+++ develop/app/src/main/java/com/devs/tutorsapp/ui/auth/LoginActivity.java
@@ -3,6 +3,7 @@ package com.devs.tutorsapp.ui.auth;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,10 +24,18 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etEmail, etPassword;
     private Button btnLogin;
     private TextView tvRegistro;
+    private CheckBox checkRemember;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (SharedPrefManager.getInstance(this).isLoggedIn()) {
+            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
 
         setContentView(R.layout.activity_login);
 
@@ -36,6 +45,7 @@ public class LoginActivity extends AppCompatActivity {
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
         tvRegistro = findViewById(R.id.tvSignUp);
+        checkRemember = findViewById(R.id.cbRememberMe);
 
         btnLogin.setOnClickListener(v -> login());
 
@@ -51,11 +61,17 @@ public class LoginActivity extends AppCompatActivity {
         authViewModel.getAlumno().observe(this, alumno -> {
             Toast.makeText(this, "Login exitoso", Toast.LENGTH_SHORT).show();
 
-            SharedPrefManager.getInstance(this).saveLogin(alumno.getAlumno_id(), alumno.getNombre(), alumno.getEmail());
+            if (checkRemember.isChecked()) {
+                SharedPrefManager.getInstance(this).saveLogin(alumno.getAlumno_id(), alumno.getNombre(), alumno.getEmail());
+            }
 
-            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+            Intent intent = new Intent(this, HomeActivity.class);
+            intent.putExtra("idAlumno", alumno.getAlumno_id());
+            intent.putExtra("nombreAlumno", alumno.getNombre());
+            intent.putExtra("emailAlumno", alumno.getEmail());
             startActivity(intent);
             finish();
+            return;
         });
     }
 
