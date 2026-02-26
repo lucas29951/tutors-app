3f98327c27abbb622e00388e80a037aeff0729d9
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-02-26 17:51:45 -0300

    Dao modificado - metodo agregado para obtener clasesDetalle segun estado e id

--- develop/app/src/main/java/com/devs/tutorsapp/data/local/dao/ClaseDao.java
+++ develop/app/src/main/java/com/devs/tutorsapp/data/local/dao/ClaseDao.java
@@ -8,6 +8,7 @@ import androidx.room.Query;
 import androidx.lifecycle.LiveData;
 import java.util.List;
 import com.devs.tutorsapp.data.local.entity.ClaseEntity;
+import com.devs.tutorsapp.data.model.ClaseDetalle;
 
 @Dao
 public interface ClaseDao {
@@ -39,4 +40,12 @@ public interface ClaseDao {
     @Query("SELECT * FROM clases WHERE estado = :estado AND alumno_id = :alumno_id")
     LiveData<List<ClaseEntity>> getClasesByEstadoAndAlumnoId(String estado, int alumno_id);
 
+    @Query("SELECT c.clase_id, c.fecha, c.hora_inicio, c.estado, " +
+            "t.nombre AS tutorNombre, t.apellido AS tutorApellido, " +
+            "m.nombre AS materiaNombre " +
+            "FROM clases c " +
+            "INNER JOIN tutores t ON c.tutor_id = t.tutor_id " +
+            "INNER JOIN materias m ON c.materia_id = m.materia_id" +
+            " WHERE c.estado = :estado AND c.alumno_id = :alumno_id")
+    LiveData<List<ClaseDetalle>> getClasesConDetalles(String estado, int alumno_id);
 }
