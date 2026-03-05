7623ca4c1a4ef6ddbf7739b91dabf72938158115
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-03-05 19:08:25 -0300

    ViewModel actualizado con los metodos de obtencion de rating de tutor

--- develop/app/src/main/java/com/devs/tutorsapp/ui/viewmodel/TutorViewModel.java
+++ develop/app/src/main/java/com/devs/tutorsapp/ui/viewmodel/TutorViewModel.java
@@ -8,8 +8,10 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.devs.tutorsapp.data.local.entity.ResenaEntity;
 import com.devs.tutorsapp.data.local.entity.TutorEntity;
 import com.devs.tutorsapp.data.repository.RepositoryCallback;
+import com.devs.tutorsapp.data.repository.ResenaRepository;
 import com.devs.tutorsapp.data.repository.TutorRepository;
 
 import java.util.List;
@@ -17,12 +19,14 @@ import java.util.List;
 public class TutorViewModel extends AndroidViewModel {
 
     private final TutorRepository repository;
+    private final ResenaRepository resenaRepository;
     private final MutableLiveData<List<TutorEntity>> listaTutores = new MutableLiveData<>();
     private final LiveData<List<TutorEntity>> allTutores;
 
     public TutorViewModel(@NonNull Application application) {
         super(application);
         repository = new TutorRepository(application);
+        resenaRepository = new ResenaRepository(application);
         allTutores = repository.getAllTutores();
     }
 
@@ -50,5 +54,17 @@ public class TutorViewModel extends AndroidViewModel {
 
     public LiveData<TutorEntity> getTutorById(int id) {
         return repository.getTutorById(id);
+    }
+
+    public LiveData<List<ResenaEntity>> getReviews(int id) {
+        return resenaRepository.getReviewsByTutor(id);
+    }
+
+    public LiveData<Float> getRating(int id) {
+        return resenaRepository.getRatingPromedio(id);
+    }
+
+    public LiveData<Integer> getCantidadResenas(int id) {
+        return resenaRepository.getCantidadResenas(id);
     }
 }
