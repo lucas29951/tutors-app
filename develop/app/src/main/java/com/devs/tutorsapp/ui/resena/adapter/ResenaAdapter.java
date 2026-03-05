d029574ce9dee67617e2d4c4a2204473866c6349
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-03-05 19:01:39 -0300

    Adapter agregado para manejo y carga de Reseñas en TutorProfile

---
+++ develop/app/src/main/java/com/devs/tutorsapp/ui/resena/adapter/ResenaAdapter.java
@@ -0,0 +1,73 @@
+package com.devs.tutorsapp.ui.resena.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.devs.tutorsapp.R;
+import com.devs.tutorsapp.data.local.entity.ResenaEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResenaAdapter extends RecyclerView.Adapter<ResenaAdapter.ViewHolder> {
+
+    private List<ResenaEntity> list = new ArrayList<>();
+
+    public void setData(List<ResenaEntity> data) {
+        list = data;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_review, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ResenaEntity item = list.get(position);
+
+        holder.tvNomAlu.setText("Alumno ID: "+ item.getAlumno_id());
+        holder.tvDateRev.setText(item.getFecha());
+        holder.tvComment.setText(item.getComentario());
+
+        holder.tvProm.setText(getStars((int) item.getPuntuacion()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list != null ? list.size() : 0;
+    }
+
+    private String getStars(int rating) {
+        if (rating <= 0) return "★";
+
+        StringBuilder stars = new StringBuilder();
+
+        for (int i = 0; i < rating; i++) {
+            stars.append("★");
+        }
+        return stars.toString();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvNomAlu, tvProm, tvComment, tvDateRev;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvNomAlu = itemView.findViewById(R.id.tvNomAlu);
+            tvDateRev = itemView.findViewById(R.id.tvDateRev);
+            tvComment = itemView.findViewById(R.id.tvComment);
+            tvProm = itemView.findViewById(R.id.tvProm);
+        }
+    }
+}
