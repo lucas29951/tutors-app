cceabb6670c44e0413741641192bfdd3f40a2de7
Lucas Rodriguez <lucasg29r95@gmail.com>
2026-03-05 19:05:21 -0300

    Metodos agregados al DAO para obtener rating del tutor

--- develop/app/src/main/java/com/devs/tutorsapp/data/local/dao/ResenaDao.java
+++ develop/app/src/main/java/com/devs/tutorsapp/data/local/dao/ResenaDao.java
@@ -30,4 +30,13 @@ public interface ResenaDao {
     @Query("SELECT * FROM resenas WHERE alumno_id = :alumno_id")
     LiveData<List<ResenaEntity>> getResenasByAlumnoId(int alumno_id);
 
+    @Query("SELECT * FROM resenas WHERE tutor_id = :tutor_id ORDER BY resena_id DESC")
+    LiveData<List<ResenaEntity>> getResenasByTutor(int tutor_id);
+
+    @Query("SELECT AVG(puntuacion) FROM resenas WHERE tutor_id = :tutor_id")
+    LiveData<Float> getRatingPromedio(int tutor_id);
+
+    @Query("SELECT COUNT(*) FROM resenas WHERE tutor_id = :tutor_id")
+    LiveData<Integer> getCantidadResenas(int tutor_id);
+
 }
